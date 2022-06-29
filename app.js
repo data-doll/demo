0718db62798784cc1dda15957b0267157ede882e
data-doll <patelrutu1997.rp@gmail.com>
2022-06-29 18:22:47 +0530

    new3

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const app = express();
 
-//change1
-//change2
-
-
 app.get("/test", (_req, res) =>  {
   res.status(200).send("Hello Data Doll , Have a nice day!!")
 })
